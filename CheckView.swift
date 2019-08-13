9508bf63485d1dcf6554cc1df2e64e2926aa0f7a
Steven Lipton <steve@makeapppie.com>
2019-08-13 07:44:14 -0500

    Initial Commit

---
+++ CheckView.swift
@@ -0,0 +1,23 @@
+//
+//  CheckView.swift
+//  Tips_08_03_CheckView
+//
+//  Created by Steven Lipton on 8/13/19.
+//  Copyright © 2019 Steven Lipton. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct CheckView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckView()
+    }
+}
+#endif
